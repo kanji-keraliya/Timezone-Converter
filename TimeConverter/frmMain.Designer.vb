d14f5cb1eef9e553d1e0c7bde4311bf3426c8e2a
Kanji Keraliya <kanji.keraliya@outlook.com>
2019-04-07 21:04:15 +0530

    Added

---
+++ TimeConverter/frmMain.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.lblProcess = New System.Windows.Forms.Label()
+        Me.groupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cmbDateFormat = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.cmbSourceTimeZone = New System.Windows.Forms.ComboBox()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.dtpDateTime = New System.Windows.Forms.DateTimePicker()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.cmbTimeZone = New System.Windows.Forms.ComboBox()
+        Me.dgvProcesses = New System.Windows.Forms.DataGridView()
+        Me.ColDelete = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.ColID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TimeZoneID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColTimeZone = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.groupBox2.SuspendLayout()
+        CType(Me.dgvProcesses, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblProcess
+        '
+        Me.lblProcess.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblProcess.AutoSize = True
+        Me.lblProcess.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProcess.Location = New System.Drawing.Point(11, 423)
+        Me.lblProcess.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblProcess.Name = "lblProcess"
+        Me.lblProcess.Size = New System.Drawing.Size(93, 16)
+        Me.lblProcess.TabIndex = 4
+        Me.lblProcess.Text = "Processing..."
+        '
+        'groupBox2
+        '
+        Me.groupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.groupBox2.Controls.Add(Me.cmbDateFormat)
+        Me.groupBox2.Controls.Add(Me.Label2)
+        Me.groupBox2.Controls.Add(Me.Label1)
+        Me.groupBox2.Controls.Add(Me.btnConvert)
+        Me.groupBox2.Controls.Add(Me.cmbSourceTimeZone)
+        Me.groupBox2.Controls.Add(Me.lblDate)
+        Me.groupBox2.Controls.Add(Me.dtpDateTime)
+        Me.groupBox2.Controls.Add(Me.btnAdd)
+        Me.groupBox2.Controls.Add(Me.cmbTimeZone)
+        Me.groupBox2.Controls.Add(Me.dgvProcesses)
+        Me.groupBox2.Controls.Add(Me.lblProcess)
+        Me.groupBox2.Location = New System.Drawing.Point(5, -2)
+        Me.groupBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.groupBox2.Name = "groupBox2"
+        Me.groupBox2.Padding = New System.Windows.Forms.Padding(4)
+        Me.groupBox2.Size = New System.Drawing.Size(775, 444)
+        Me.groupBox2.TabIndex = 0
+        Me.groupBox2.TabStop = False
+        '
+        'cmbDateFormat
+        '
+        Me.cmbDateFormat.DropDownHeight = 200
+        Me.cmbDateFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDateFormat.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbDateFormat.FormattingEnabled = True
+        Me.cmbDateFormat.IntegralHeight = False
+        Me.cmbDateFormat.Items.AddRange(New Object() {"dd-MMM-yyyy hh:mm:ss tt", "dd-MMM-yyyy HH:mm:ss", "dd-MM-yyyy hh:mm:ss tt", "dd-MM-yyyy HH:mm:ss", "MM-dd-yyyy hh:mm:ss tt", "MM-dd-yyyy HH:mm:ss", "MM-dd-yy hh:mm:ss tt", "MM-dd-yy HH:mm:ss", "dd-MMM-yyyy hh:mm:ss tt,ddd", "dd-MMM-yyyy HH:mm:ss,ddd", "dd-MM-yyyy hh:mm:ss tt,dddd", "dd-MM-yyyy HH:mm:ss,dddd"})
+        Me.cmbDateFormat.Location = New System.Drawing.Point(377, 13)
+        Me.cmbDateFormat.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmbDateFormat.Name = "cmbDateFormat"
+        Me.cmbDateFormat.Size = New System.Drawing.Size(274, 22)
+        Me.cmbDateFormat.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(325, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 14)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Format"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 14)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Time Zone"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(658, 40)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(106, 26)
+        Me.btnConvert.TabIndex = 3
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'cmbSourceTimeZone
+        '
+        Me.cmbSourceTimeZone.DropDownHeight = 300
+        Me.cmbSourceTimeZone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSourceTimeZone.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbSourceTimeZone.FormattingEnabled = True
+        Me.cmbSourceTimeZone.IntegralHeight = False
+        Me.cmbSourceTimeZone.Location = New System.Drawing.Point(102, 40)
+        Me.cmbSourceTimeZone.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmbSourceTimeZone.Name = "cmbSourceTimeZone"
+        Me.cmbSourceTimeZone.Size = New System.Drawing.Size(549, 22)
+        Me.cmbSourceTimeZone.TabIndex = 2
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(11, 17)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(83, 14)
+        Me.lblDate.TabIndex = 8
+        Me.lblDate.Text = "Date && Time"
+        '
+        'dtpDateTime
+        '
+        Me.dtpDateTime.CustomFormat = "dd-MMM-yyyy hh:mm:ss tt"
+        Me.dtpDateTime.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpDateTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpDateTime.Location = New System.Drawing.Point(102, 14)
+        Me.dtpDateTime.Name = "dtpDateTime"
+        Me.dtpDateTime.Size = New System.Drawing.Size(217, 22)
+        Me.dtpDateTime.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(658, 87)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(106, 26)
+        Me.btnAdd.TabIndex = 5
+        Me.btnAdd.Text = "Add New"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'cmbTimeZone
+        '
+        Me.cmbTimeZone.DropDownHeight = 300
+        Me.cmbTimeZone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbTimeZone.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbTimeZone.FormattingEnabled = True
+        Me.cmbTimeZone.IntegralHeight = False
+        Me.cmbTimeZone.Location = New System.Drawing.Point(11, 87)
+        Me.cmbTimeZone.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmbTimeZone.Name = "cmbTimeZone"
+        Me.cmbTimeZone.Size = New System.Drawing.Size(640, 22)
+        Me.cmbTimeZone.TabIndex = 4
+        '
+        'dgvProcesses
+        '
+        Me.dgvProcesses.AllowUserToAddRows = False
+        Me.dgvProcesses.AllowUserToDeleteRows = False
+        Me.dgvProcesses.AllowUserToOrderColumns = True
+        Me.dgvProcesses.AllowUserToResizeRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.dgvProcesses.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvProcesses.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvProcesses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvProcesses.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColDelete, Me.ColID, Me.TimeZoneID, Me.ColTimeZone, Me.DateTime})
+        Me.dgvProcesses.Location = New System.Drawing.Point(11, 114)
+        Me.dgvProcesses.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgvProcesses.Name = "dgvProcesses"
+        Me.dgvProcesses.ReadOnly = True
+        Me.dgvProcesses.RowHeadersVisible = False
+        Me.dgvProcesses.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvProcesses.Size = New System.Drawing.Size(753, 307)
+        Me.dgvProcesses.TabIndex = 6
+        '
+        'ColDelete
+        '
+        Me.ColDelete.HeaderText = ""
+        Me.ColDelete.Image = Global.TimeZoneConverter.My.Resources.Resources.DeleteRed
+        Me.ColDelete.Name = "ColDelete"
+        Me.ColDelete.ReadOnly = True
+        Me.ColDelete.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.ColDelete.ToolTipText = "Delete"
+        Me.ColDelete.Width = 30
+        '
+        'ColID
+        '
+        Me.ColID.HeaderText = "ID"
+        Me.ColID.Name = "ColID"
+        Me.ColID.ReadOnly = True
+        Me.ColID.Visible = False
+        '
+        'TimeZoneID
+        '
+        Me.TimeZoneID.DataPropertyName = "TimeZoneId"
+        Me.TimeZoneID.HeaderText = "Time Zone ID"
+        Me.TimeZoneID.Name = "TimeZoneID"
+        Me.TimeZoneID.ReadOnly = True
+        Me.TimeZoneID.ToolTipText = "Time Zone ID"
+        Me.TimeZoneID.Width = 180
+        '
+        'ColTimeZone
+        '
+        Me.ColTimeZone.DataPropertyName = "TimeZoneName"
+        Me.ColTimeZone.HeaderText = "Time Zone"
+        Me.ColTimeZone.Name = "ColTimeZone"
+        Me.ColTimeZone.ReadOnly = True
+        Me.ColTimeZone.ToolTipText = "Time zone display name"
+        Me.ColTimeZone.Width = 320
+        '
+        'DateTime
+        '
+        Me.DateTime.DataPropertyName = "DateTime"
+        Me.DateTime.HeaderText = "Date & Time"
+        Me.DateTime.Name = "DateTime"
+        Me.DateTime.ReadOnly = True
+        Me.DateTime.ToolTipText = "Date And Time"
+        Me.DateTime.Width = 190
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(785, 447)
+        Me.Controls.Add(Me.groupBox2)
+        Me.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Time Zone Converter"
+        Me.groupBox2.ResumeLayout(False)
+        Me.groupBox2.PerformLayout()
+        CType(Me.dgvProcesses, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents lblProcess As System.Windows.Forms.Label
+    Private WithEvents groupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents dgvProcesses As System.Windows.Forms.DataGridView
+    Friend WithEvents cmbTimeZone As System.Windows.Forms.ComboBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents dtpDateTime As System.Windows.Forms.DateTimePicker
+    Friend WithEvents lblDate As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents cmbSourceTimeZone As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmbDateFormat As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ColDelete As System.Windows.Forms.DataGridViewImageColumn
+    Friend WithEvents ColID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TimeZoneID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColTimeZone As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DateTime As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ TimeConverter/frmMain.vb
@@ -0,0 +1,262 @@
+﻿Imports System.IO
+
+Public Class frmMain
+
+#Region "Form Related Code"
+    Private m_strDateTimeFormat As String = "dd-MMM-yyyy hh:mm:ss tt"
+
+    Private Sub frmMain_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Try
+            'My.Settings.Item("TimeZone") = cmbSourceTimeZone.SelectedValue
+            'My.Settings.Item("DateTimeFormat") = cmbDateFormat.Text
+            'My.Settings.Save()
+
+            My.Settings.TimeZone = cmbSourceTimeZone.SelectedValue
+            My.Settings.DateTimeFormat = cmbDateFormat.Text
+            My.Settings.Save()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            Me.Icon = My.Resources.world_clock
+            cmbDateFormat.SelectedIndex = 0
+            pLoadSystemTimeZone()
+            pLoadTimeZones()
+
+            Dim sSavedTimeFormat As String = My.Settings.DateTimeFormat.ToString()
+            Dim sSavedTimeZone As String = My.Settings.TimeZone.ToString()
+
+            If String.IsNullOrEmpty(sSavedTimeFormat) = False Then cmbDateFormat.Text = sSavedTimeFormat
+            If String.IsNullOrEmpty(sSavedTimeZone) = False Then cmbSourceTimeZone.SelectedValue = sSavedTimeZone
+
+            pConvertTime()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        Try
+            If cmbTimeZone.SelectedIndex <> -1 Then
+                Dim dr() As DataRow = m_dsTimeZones.Tables(0).Select("TimeZoneID='" & cmbTimeZone.SelectedValue & "'")
+                If dr.Length > 0 Then
+                    SetProcessLabel("TimeZone already exist")
+                    Exit Sub
+                End If
+                pAddNewTimeZone(cmbTimeZone.SelectedValue, cmbTimeZone.Text)
+
+                pSaveTimeZone()
+                SetProcessLabel("Completed")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Private Sub btnConvert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConvert.Click
+        Try
+            pConvertTime()
+            SetProcessLabel("Completed")
+        Catch ex As Exception
+            SetProcessLabel("Error : " & ex.Message)
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Private Sub dgvProcesses_CellClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvProcesses.CellClick
+        Try
+            If e.RowIndex <> -1 AndAlso dgvProcesses.Columns(e.ColumnIndex).Name = "ColDelete" Then
+
+                Dim dr() As DataRow = m_dsTimeZones.Tables(0).Select("TimeZoneID='" & dgvProcesses.Rows(e.RowIndex).Cells("TimeZoneId").Value & "'")
+                If dr.Length > 0 Then
+                    dr(0).Delete()
+                End If
+
+                pSaveTimeZone()
+                SetProcessLabel("Completed")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+
+    Private Sub cmbDateFormat_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbDateFormat.SelectedIndexChanged
+        Try
+            If cmbDateFormat.Text IsNot Nothing AndAlso String.IsNullOrEmpty(cmbDateFormat.Text) = False Then
+                m_strDateTimeFormat = cmbDateFormat.Text
+                dtpDateTime.CustomFormat = m_strDateTimeFormat
+                pConvertTime()
+                SetProcessLabel("Completed")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, My.Application.Info.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
+#End Region
+
+#Region "Function and Procedures"
+    Private m_dsTimeZones As New DataSet
+    Dim m_DataFilePath As String = Path.Combine(Application.StartupPath, My.Application.Info.AssemblyName & "_Data.xml")
+
+    Private Sub pLoadTimeZones()
+        Try
+            SetProcessLabel("Fetching process list")
+            If m_dsTimeZones Is Nothing Then m_dsTimeZones = New DataSet
+
+            If File.Exists(m_DataFilePath) Then
+                m_dsTimeZones = New DataSet
+                m_dsTimeZones.ReadXml(m_DataFilePath, XmlReadMode.Auto)
+            Else
+                Dim dtTimeZone As New DataTable("TimeZones")
+                dtTimeZone.Columns.Add("ID", GetType(System.Int64))
+                dtTimeZone.Columns.Add("TimeZoneId", GetType(System.String))
+                dtTimeZone.Columns.Add("TimeZoneName", GetType(System.String))
+                dtTimeZone.Columns.Add("DateTime", GetType(System.String))
+                m_dsTimeZones.Tables.Add(dtTimeZone)
+            End If
+
+            If m_dsTimeZones.Tables("TimeZones").Rows.Count = 0 Then
+                Dim tz As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(TimeZone.CurrentTimeZone.StandardName)
+                pAddNewTimeZone(tz.Id, tz.DisplayName & " (" & tz.Id & ")")
+            End If
+
+            dgvProcesses.AutoGenerateColumns = False
+            dgvProcesses.DataSource = m_dsTimeZones.Tables("TimeZones")
+            'dgvProcesses.Refresh()
+
+            pConvertTime()
+        Catch ex As Exception
+            Throw ex
+        Finally
+            SetProcessLabel("")
+        End Try
+    End Sub
+
+    Private Sub pLoadSystemTimeZone()
+        Try
+            SetProcessLabel("Loading system time zones")
+            Dim dtTimeZone As New DataTable
+            dtTimeZone.Columns.Add("TimeZoneId", GetType(System.String))
+            dtTimeZone.Columns.Add("TimeZoneName", GetType(System.String))
+
+            For Each tz As TimeZoneInfo In TimeZoneInfo.GetSystemTimeZones()
+                Dim dr As DataRow = dtTimeZone.NewRow
+                dr("TimeZoneID") = tz.Id
+                dr("TimeZoneName") = tz.DisplayName & " (" & tz.Id & ")"
+                dtTimeZone.Rows.Add(dr)
+            Next
+
+            cmbTimeZone.DataSource = dtTimeZone.Copy
+            cmbTimeZone.ValueMember = "TimeZoneId"
+            cmbTimeZone.DisplayMember = "TimeZoneName"
+
+            cmbSourceTimeZone.DataSource = dtTimeZone.Copy
+            cmbSourceTimeZone.ValueMember = "TimeZoneId"
+            cmbSourceTimeZone.DisplayMember = "TimeZoneName"
+
+
+            cmbTimeZone.SelectedValue = TimeZone.CurrentTimeZone.StandardName
+            cmbSourceTimeZone.SelectedValue = TimeZone.CurrentTimeZone.StandardName
+
+        Catch ex As Exception
+            Throw ex
+        Finally
+            SetProcessLabel("")
+        End Try
+    End Sub
+
+    Private Sub SetProcessLabel(ByVal Msg As String)
+        Try
+            lblProcess.Text = Msg
+            Application.DoEvents()
+        Catch ex As Exception
+            'Throw ex
+        End Try
+    End Sub
+
+    Private Sub pAddNewTimeZone(ByVal TimeZoneId As String, ByVal TimeZoneName As String)
+        Try
+            SetProcessLabel("Adding New time zone")
+
+            With m_dsTimeZones.Tables("TimeZones")
+                Dim iNextId = .Compute("Max(Id)", "")
+                If IsDBNull(iNextId) Then
+                    iNextId = 1
+                Else
+                    iNextId += 1
+                End If
+
+                Dim dr As DataRow = .NewRow
+                dr("ID") = iNextId
+                dr("TimeZoneId") = TimeZoneId
+                dr("TimeZoneName") = TimeZoneName
+
+                Dim TimeZoneDatetime As DateTime = fnConvertTimeByTimeZone(TimeZoneId)
+                If TimeZoneDatetime <> Date.MinValue Then
+                    dr("DateTime") = TimeZoneDatetime.ToString(m_strDateTimeFormat)
+                    'dr("DateTime") = TimeZoneDatetime.ToString("dd-MMM-yyyy hh:mm:ss tt")
+                End If
+                .Rows.Add(dr)
+                SetProcessLabel("New timezone added successfully")
+            End With
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub pSaveTimeZone()
+        Try
+            SetProcessLabel("Save timezones")
+            If m_dsTimeZones IsNot Nothing AndAlso m_dsTimeZones.Tables.Count > 0 Then
+                m_dsTimeZones.WriteXml(m_DataFilePath, XmlWriteMode.WriteSchema)
+
+                pLoadTimeZones()
+            End If
+            SetProcessLabel("Save timezones successfully")
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub pConvertTime()
+        Try
+            For Each dr As DataGridViewRow In dgvProcesses.Rows
+                Dim TimeZoneDatetime As DateTime = fnConvertTimeByTimeZone(dr.Cells("TimeZoneID").Value)
+                If TimeZoneDatetime = Date.MinValue Then
+                    dr.Cells("DateTime").Value = Nothing
+                Else
+                    dr.Cells("DateTime").Value = TimeZoneDatetime.ToString(m_strDateTimeFormat)
+                    'dr.Cells("DateTime").Value = TimeZoneDatetime.ToString("dd-MMM-yyyy hh:mm:ss tt")
+                End If
+                'dr.Cells("DateTime").Value = fnConvertTimeByTimeZone(dr.Cells("TimeZoneID").Value).ToString("dd-MMM-yyyy hh:mm:ss tt")
+            Next
+            dgvProcesses.Refresh()
+            SetProcessLabel("Completed")
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Function fnConvertTimeByTimeZone(ByVal TimeZoneId As String) As DateTime
+        Try
+            SetProcessLabel("Converting time for " & TimeZoneId)
+            Dim sourceTimeZone As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(cmbSourceTimeZone.SelectedValue)
+            Dim DestinationTimeZone As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(TimeZoneId)
+            Dim dtmDateTime As DateTime = dtpDateTime.Value
+            dtmDateTime = System.DateTime.SpecifyKind(dtmDateTime, DateTimeKind.Unspecified)
+            If sourceTimeZone.IsInvalidTime(dtmDateTime) Then
+                Throw New Exception("The time " & dtmDateTime.ToString("dd-MMM-yyyy hh:mm:ss tt") & " does not exist in " & cmbSourceTimeZone.SelectedValue & ".It could be because Daylight Saving Time skipped one hour, or a time zone change. Please try with an hour earlier or later.")
+                dtmDateTime = dtmDateTime.AddHours(1)
+            End If
+            Dim result As DateTime = TimeZoneInfo.ConvertTime(dtmDateTime, sourceTimeZone, DestinationTimeZone)
+            Return result
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+#End Region
+
+End Class
